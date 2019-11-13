d8eeae303cadb370bc596406b3c8f412188e3922
riccardojohnson <50954349+riccardojohnson@users.noreply.github.com>
2019-11-13 14:05:45 +0000

    Update TestScript.ps1

--- TestScript.ps1
+++ TestScript.ps1
@@ -1,14 +1,1 @@
-﻿$Computer = Get-ADComputer -Identity GBRDCWNTD001 #-Filter * -SearchBase "OU=Servers,DC=emea,DC=cbre,DC=net"
-$Computers = $Computer.Name
-Foreach($psitem in $Computers)
-{
-Get-WindowsFeature -ComputerName $psitem -Name Failover* | where InstallState -Eq Installed | select name
-}
-If ($Computer.Name -eq "GBRDCWNTD001") {
-  'This server is GBRDCWNTD001'
-
-  }  Else {
-
-  'This server is not GBRDCWNTD001'
-
-} 
+Write-Host -ForegroundColor Cyan HELLO
